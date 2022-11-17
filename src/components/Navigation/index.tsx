6f0dd63e018f9124973922915c6f8053ea0613fc
nam <70477669+ducnam0902@users.noreply.github.com>
2022-11-17 16:46:31 +0700

    Config/Project structure  (#1)

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+import { Outlet } from "react-router-dom";
+import Footer from "src/components/Footer";
+import Header from "src/components/Header";
+
+const Navigation = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+Navigation.propTypes = {
+  children: PropTypes.node,
+};
+
+export default Navigation
