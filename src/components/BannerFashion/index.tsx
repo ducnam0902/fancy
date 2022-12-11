db28db2952ac20e4e7047fc635a9fac4150acd09
nam <70477669+ducnam0902@users.noreply.github.com>
2022-12-11 20:04:57 +0700

    Feat/categories (#5)

---
+++ src/components/BannerFashion/index.tsx
@@ -0,0 +1,41 @@
+import styles from "./styles";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Image from "mui-image";
+import banner1 from "src/assets/banner1.webp";
+import banner2 from "src/assets/banner2.webp";
+import banner3 from "src/assets/banner3.webp";
+import { Box, Typography } from "@mui/material";
+import FancyButton from "src/components/FancyButton";
+
+const BannerFashion = () => {
+  return (
+    <Container>
+      <Grid container spacing={2} sx={styles.gridStyled}>
+        <Grid item xs={12} lg={6}>
+          <Box sx={styles.boxContainer}>
+            <Image src={banner1} wrapperStyle={styles.imageWrapper} style={styles.image} />
+            <Typography variant="h3" sx={styles.menFashion}>
+              Men's Fashion
+            </Typography>
+            <FancyButton style={styles.shop}>Shop Now</FancyButton>
+          </Box>
+        </Grid>
+        <Grid item xs={12} lg={6}>
+          <Box sx={styles.boxContainer}>
+            <Image src={banner2} wrapperStyle={styles.accessories} style={styles.image} />
+            <Typography variant="h3" sx={styles.menFashion}>Accessories</Typography>
+            <FancyButton style={styles.shopJewelry}>Shop Now</FancyButton>
+          </Box>
+          <Box sx={styles.boxContainer}>
+            <Image src={banner3} wrapperStyle={styles.imageWrapper} style={styles.image} />
+            <Typography variant="h3" sx={styles.womenFashion}>Women's Fashion</Typography>
+            <FancyButton style={styles.shopWomen}>Shop Now</FancyButton>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default BannerFashion;
---
+++ src/components/BannerFashion/styles.ts
@@ -0,0 +1,95 @@
+export const styles = {
+    gridStyled:{
+        marginTop: 4,
+        marginBottom: 4,
+    },
+    imageWrapper:{
+        display: "block",
+        height: "auto",
+    },
+    image: {
+        height: "100%",
+    },
+    accessories:{
+        display: "block",
+        height: "auto",
+        marginBottom: '23px',
+    },
+    boxContainer: {
+        position: 'relative'
+    },
+    menFashion:{
+        position: 'absolute',
+        top: '40%',
+        right: '10%',
+        fontFamily: 'Lato',
+        fontWeight: 700,
+        fontSize: {
+            md: '25px',
+            xs: '14px',
+        },
+        lineHeight: '28px',
+        textTransform: 'uppercase',
+        color: '#444',
+        letterSpacing: '5px'
+    },
+    womenFashion:{
+        position: 'absolute',
+        top: '40%',
+        left: '5%',
+        fontFamily: 'Lato',
+        fontWeight: 700,
+        fontSize: {
+            md: '25px',
+            xs: '14px',
+        },
+        lineHeight: '28px',
+        textTransform: 'uppercase',
+        color: '#444',
+        letterSpacing: '3px'
+    },
+    shop:{
+        width: '30%',
+        position: 'absolute',
+        top: '48%',
+        right: '10%',
+        backgroundColor: 'transparent',
+        border: '1px solid #444',
+        color: '#444',
+        padding: '12px',
+        fontSize: {
+            xs: '8px',
+            sm: '16px',
+        }
+    },
+    shopJewelry:{
+        width: '30%',
+        position: 'absolute',
+        top: '55%',
+        right: '10%',
+        backgroundColor: 'transparent',
+        border: '1px solid #444',
+        color: '#444',
+        padding: '12px',
+        fontSize: {
+            xs: '8px',
+            sm: '16px',
+        }
+    },
+    shopWomen:{
+        width: '30%',
+        position: 'absolute',
+        top: '55%',
+        left: '5%',
+        backgroundColor: 'transparent',
+        border: '1px solid #444',
+        color: '#444',
+        padding: '12px',
+        fontSize: {
+            xs: '8px',
+            sm: '16px',
+        }
+    },
+};
+
+export default styles;
