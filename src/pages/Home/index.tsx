e40199350db36d97a89e828443ab517542290e73
nam <ducnam0902@gmail.com>
2022-12-11 20:03:59 +0700

    chore: feat/categories

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
+import BannerFashion from "src/components/BannerFashion"
 import Slide from "src/components/Slide"
 
 const Home = () => {
   return (
     <>
       <Slide/>
+      <BannerFashion/>
     </>
   )
 }
