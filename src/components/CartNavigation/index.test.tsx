e38025cf66b19cab0c9777ea05279cefff7d0c9d
nam <70477669+ducnam0902@users.noreply.github.com>
2023-01-01 22:13:30 +0700

    Feat/ Integrate with Jenkins (#9)

--- src/components/CartNavigation/index.test.tsx
+++ src/components/CartNavigation/index.test.tsx
@@ -6,7 +6,6 @@ describe('Should render Cart Navigation successfully', () =>{
   test('Should show the Box after click Shopping Cart icon', () =>{
     render(<CartNavigation/>)
     expect(screen.queryByText(/Check out/)).toBeNull();
-    const listItem = screen.queryByRole('listitem');
     userEvent.click(screen.queryByRole('listitem') as Element);
     expect(screen.queryByText(/Check out/)).toBeInTheDocument();
 
